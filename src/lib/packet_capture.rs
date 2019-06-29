62dd1989e9e67835b5aa37416602b8aa24589fc5
kanishkarj <kanishkarj@hotmail.com>
2019-06-29 22:59:30 +0530

    Attempt parsing packets till from link to transport layer

--- src/lib/packet_capture.rs
+++ src/lib/packet_capture.rs
@@ -2,7 +2,48 @@ use pcap::{Device, Capture, Inactive, Active};
 use std::fmt::{Display, Formatter};
 use std::path::Path;
 
+use packet::{ether, tcp, udp, ip, icmp};
+use packet::Packet;
 pub struct PacketCapture {}
+
+fn parse_packet(content: &[u8]) {
+    if let Ok(content)  = ether::Packet::new(content) {
+        println!("ether");
+        match content.protocol() {
+            ether::Protocol::Ipv4 => {
+                if let Ok(content) = ip::v4::Packet::new(content.payload()) {
+                    println!("ipv4");
+                    match content.protocol() {
+                        ip::Protocol::Icmp => {
+                            if let Ok(content) = icmp::Packet::new(content.payload()) {
+                                println!("icmp");
+                            }
+                        },
+                        ip::Protocol::Tcp => {
+                            if let Ok(content) = tcp::Packet::new(content.payload()) {
+                                println!("{:?}", content);
+                            }
+                        },
+                        ip::Protocol::Udp => {
+                            if let Ok(content) = udp::Packet::new(content.payload()) {
+                                println!("{:?}", content);
+                            }
+                        },
+                        _ => {}
+                    }
+                }
+            },
+            ether::Protocol::Ipv6 => {
+                if let Ok(content) = ip::v6::Packet::new(content.payload()) {
+                    println!("ipv6");
+                }
+            },
+            _ => {}
+        }
+    }
+
+    println!("");
+}
 
 impl PacketCapture {
 
@@ -40,7 +81,8 @@ impl PacketCapture {
         match Capture::from_file(file_name) {
             Ok(mut cap_handle) => {
                 while let Ok(packet) = cap_handle.next() {
-                    println!("{:?}", packet);
+//                    println!("{:?}", packet);
+                    parse_packet(packet.data);
                 }
             },
             Err(err) => {
