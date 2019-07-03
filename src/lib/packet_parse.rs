b4a16dd817b0ddd9b010be4b3b5bb37e7401d8d2
kanishkarj <kanishkarj@hotmail.com>
2019-07-03 11:57:58 +0530

    Get TLS parsing working

--- src/lib/packet_parse.rs
+++ src/lib/packet_parse.rs
@@ -6,13 +6,15 @@ use pktparse::arp::ArpPacket;
 use pktparse::tcp::TcpHeader;
 use pktparse::udp::UdpHeader;
 use pktparse::ipv6::IPv6Header;
+use tls_parser::TlsMessage;
 
 use serde::{Deserialize, Serialize};
 
 pub struct PacketParse {}
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub enum PacketHeader {
+    Tls(TlsType),
     Tcp(TcpHeader),
     Udp(UdpHeader),
     Ipv4(IPv4Header),
@@ -23,8 +25,8 @@ pub enum PacketHeader {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ParsedPacket {
-    headers: Vec<PacketHeader>,
-    remaining: Vec<u8>,
+    pub headers: Vec<PacketHeader>,
+    pub remaining: Vec<u8>,
 }
 
 impl ParsedPacket {
@@ -34,6 +36,15 @@ impl ParsedPacket {
             remaining: vec![]
         }
     }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum TlsType {
+    Handshake,
+    ChangeCipherSpec,
+    Alert,
+    ApplicationData,
+    Heartbeat,
 }
 
 impl PacketParse {
@@ -50,7 +61,7 @@ impl PacketParse {
         match  ethernet::parse_ethernet_frame(content) {
             Ok((content, headers)) => {
                 match headers.ethertype {
-                EtherType::IPv4 => {
+                EtherType::IPv4 | EtherType::Other(0) => {
                     self.parse_ipv4(content,&mut pack);
                 },
                 EtherType::IPv6 => {
@@ -75,7 +86,7 @@ impl PacketParse {
     pub fn parse_ipv4(&self, content: &[u8], parsed_packet: &mut ParsedPacket) {
         match  ipv4::parse_ipv4_header(content) {
             Ok((content, headers)) => {
-                self.parse_ip(&headers.protocol, content, parsed_packet);
+                self.parse_ip(content, parsed_packet);
                 parsed_packet.headers.push(PacketHeader::Ipv4(headers));
             },
             Err(_) => {
@@ -87,7 +98,7 @@ impl PacketParse {
     pub fn parse_ipv6(&self, content: &[u8], parsed_packet: &mut ParsedPacket) {
         match  ipv6::parse_ipv6_header(content) {
             Ok((content, headers)) => {
-                self.parse_ip(&headers.next_header, content, parsed_packet);
+                self.parse_ip(content, parsed_packet);
                 parsed_packet.headers.push(PacketHeader::Ipv6(headers));
             },
             Err(_) => {
@@ -96,7 +107,7 @@ impl PacketParse {
         }
     }
 
-    fn parse_ip(&self, protocol: &IPProtocol, content: &[u8], parsed_packet: &mut ParsedPacket) {
+    fn parse_ip(&self, content: &[u8], parsed_packet: &mut ParsedPacket) {
         if let Ok(_) = self.parse_tcp(content,parsed_packet) {}
         else if let Ok(_) = self.parse_udp(content,parsed_packet) {}
         else {
@@ -107,8 +118,8 @@ impl PacketParse {
     fn parse_tcp(&self, content: &[u8], parsed_packet: &mut ParsedPacket) -> Result<(),()> {
         match tcp::parse_tcp_header(content) {
             Ok((content, headers)) => {
+                self.parse_tls(content, parsed_packet);
                 parsed_packet.headers.push(PacketHeader::Tcp(headers));
-                parsed_packet.remaining = content.to_owned();
                 Ok(())
             }
             Err(_) => {
@@ -142,4 +153,41 @@ impl PacketParse {
             }
         }
     }
+
+    fn parse_tls(&self, content: &[u8], parsed_packet: &mut ParsedPacket) {
+        match tls_parser::parse_tls_plaintext(content) {
+            Ok((content, headers)) => {
+                // Here we return after parsing one msg as we do not want multiple TLS headers, maybe there is a better approach.
+                for msg in headers.msg {
+                    match msg {
+                        TlsMessage::Handshake(_) => {
+                            parsed_packet.headers.push(PacketHeader::Tls(TlsType::Handshake));
+                            return;
+                        },
+                        TlsMessage::ApplicationData(app_data) => {
+                            parsed_packet.headers.push(PacketHeader::Tls(TlsType::ApplicationData));
+                            parsed_packet.remaining = app_data.blob.to_owned();
+                            return;
+                        },
+                        TlsMessage::Heartbeat(_) => {
+                            parsed_packet.headers.push(PacketHeader::Tls(TlsType::Heartbeat));
+                            return;
+                        },
+                        TlsMessage::ChangeCipherSpec => {
+                            parsed_packet.headers.push(PacketHeader::Tls(TlsType::ChangeCipherSpec));
+                            return;
+                        },
+                        TlsMessage::Alert(_) => {
+                            parsed_packet.headers.push(PacketHeader::Tls(TlsType::Alert));
+                            return;
+                        },
+                    }
+                }
+            }
+            Err(_) => {
+                parsed_packet.remaining = content.to_owned();
+            }
+        }
+    }
+
 }
